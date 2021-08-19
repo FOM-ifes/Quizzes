b839497b9166df9d83470b7e18649b98e1d36a23
Matthias Gehrke <matthias.gehrke@fom.de>
2021-08-19 21:17:57 +0200

    nicht elegant, aber vielleicht brauchbar

---
+++ create_xmls.R
@@ -0,0 +1,89 @@
+# Quizze
+
+library(exams)
+
+wiederholung1 <- c("Grundlagen-Wissenschaft/Schlusstechnik.Rmd",
+                   "Grundlagen-Wissenschaft/Hypothese.Rmd", 
+                   "Grundlagen-Wissenschaft/Validitaet.Rmd")
+
+wiederholung2 <- c("Grundlagen-QuantitativeDatenanalyse/Guetekriterium-Messung.Rmd",
+                   "Grundlagen-QuantitativeDatenanalyse/Skala-PLZ.Rmd",
+                   "Grundlagen-QuantitativeDatenanalyse/Stichprobe.Rmd")
+
+wiederholung3 <- c("Grundlagen-QuantitativeDatenanalyse/Typ-Motivation.Rmd",
+                   "Grundlagen-QuantitativeDatenanalyse/Skala-Nutzungsdauer.Rmd",
+                   "Grundlagen-QuantitativeDatenanalyse/Typ-Anzahl.Rmd",
+                   "Grundlagen-QuantitativeDatenanalyse/Stichprobe-Cloud.Rmd",
+                   "Grundlagen-QuantitativeDatenanalyse/Schlussmoeglichkeit.Rmd")
+
+wiederholungR <- c("R/gfpoint.Rmd", 
+                   "R/mean.Rmd", 
+                   "R/Zuweisung.Rmd", 
+                   "R/Funktion.Rmd", 
+                   "R/Funktionsargument.Rmd")
+
+wiederholung4 <- c("EDA/Titanic-Beobachtung.Rmd", 
+                   "EDA/Titanic-Variable.Rmd",
+                   "EDA/Titanic-Skalenniveau1.Rmd", 
+                   "EDA/Titanic-Skalenniveau2.Rmd",
+                   "EDA/Titanic-Verteilungsform.Rmd", 
+                   "EDA/Titanic-Verteilungsfunktion.Rmd")
+
+wiederholung5 <- c("EDA/Titanic-Mittelwert.Rmd", 
+                   "EDA/Titanic-Varianz.Rmd", 
+                   "EDA/Titanic-Boxplot.Rmd", 
+                   "EDA/Titanic-Korrelation.Rmd")
+
+wiederholung6 <- c("Normalverteilung/Parameter.Rmd", 
+                   "Normalverteilung/Wkeit.Rmd", 
+                   "Normalverteilung/Quantile.Rmd", 
+                   "Normalverteilung/z-Wert.Rmd")
+
+wiederholung7 <- c("Regression/Modellgleichung.Rmd",
+                   "Regression/Korrelation.Rmd", 
+                   "Regression/KorrelationRegression.Rmd", 
+                   "Regression/Regressionsgleichung.Rmd", 
+                   "Regression/yDach.Rmd", 
+                   "Regression/InterpretationR2.Rmd", 
+                   "Regression/EinordungR2.Rmd")
+
+wiederholung8 <- c("Regression/Nullhypothese.Rmd", 
+                   "Regression/Stichprobe.Rmd", 
+                   "Regression/Bootstrapping.Rmd",
+                   "Regression/SimulationH0.Rmd", 
+                   "Regression/Signifikanz.Rmd")
+
+wiederholung9 <- c("Regression/KategorialesX.Rmd", 
+                   "Regression/GleichungKategorialsX.Rmd", 
+                   "Regression/NullhypotheseKategorialesX.Rmd", 
+                   "Regression/PWert.Rmd",
+                   "Regression/Targeting.Rmd")
+
+wiederholung10 <- c("Inferenz/Parameter.Rmd", 
+                    "Inferenz/ParameterStipro.Rmd", 
+                    "Inferenz/Schaetzer.Rmd", 
+                    "Inferenz/SE.Rmd", 
+                    "Inferenz/Konfidenzintervall.Rmd")
+
+wiederholung11 <- c("Inferenz/Nullhypothese.Rmd", 
+                    "Inferenz/pZweifel.Rmd", 
+                    "Inferenz/pTeststatistik.Rmd", 
+                    "Inferenz/Testentscheidung.Rmd", 
+                    "Inferenz/Fehler.Rmd")
+
+wiederholung12 <- c("Offene-Fragen/Histogramm.Rmd", 
+                    "Offene-Fragen/Inferenz.Rmd")
+
+exams2moodle(wiederholung1, name = "Wiederholung1", rule = "none")
+exams2moodle(wiederholung2, name = "Wiederholung2", rule = "none")
+exams2moodle(wiederholung3, name = "Wiederholung3", rule = "none")
+exams2moodle(wiederholung4, name = "Wiederholung4", rule = "none")
+exams2moodle(wiederholung5, name = "Wiederholung5", rule = "none")
+exams2moodle(wiederholung6, name = "Wiederholung6", rule = "none")
+exams2moodle(wiederholung7, name = "Wiederholung7", rule = "none")
+exams2moodle(wiederholung8, name = "Wiederholung8", rule = "none")
+exams2moodle(wiederholung9, name = "Wiederholung9", rule = "none")
+exams2moodle(wiederholung10, name = "Wiederholung10", rule = "none")
+exams2moodle(wiederholung11, name = "Wiederholung11", rule = "none")
+exams2moodle(wiederholung12, name = "Wiederholung12", rule = "none")
+exams2moodle(wiederholungR, name = "WiederholungR", rule = "none")
